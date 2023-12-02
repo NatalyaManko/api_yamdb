08332958d2e8b02362fd2d1e4f4169ff638d17f4
Rinat Saifullin <saifullinrg@yandex.ru>
2023-12-02 11:51:19 +0500

    add urls.py init views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+   pass
+
+
+class GenresViewSet(viewsets.ModelViewSet):
+    pass
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    pass
+
+
 
 # Create your views here.
